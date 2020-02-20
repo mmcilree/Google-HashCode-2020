bf71ee9ab1fd0e5edbd7ac45a226551ae8a060ca
Carson Brown <cmb38@st-andrews.ac.uk>
2020-02-20 19:10:19 +0000

    Push

--- src/actual/ActualOptimiser.java
+++ src/actual/ActualOptimiser.java
@@ -3,25 +3,47 @@ package actual;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 
 import common.Optimiser;
 import common.TabularData;
 
 public class ActualOptimiser implements Optimiser {
 
+    public class Book implements Comparable {
+        int ID;
+        int value;
+
+        public Book(int ID, int value) {
+            this.ID = ID;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            if (o instanceof Book) {
+                return Integer.compare(((Book) o).value, this.value);
+            }
+            return 0;
+        }
+    }
+
     public class Library implements Comparable {
         int numBooks;
         int numDays;
         int numShip;
         int totalVal;
+        HashSet<Book> books;
         ArrayList<Integer> bookIDs;
 
         public void calculateTotalVal() {
             totalVal = 0;
-            for (int i : bookIDs) {
-                totalVal += bookVals.get(i);
+            for (Book b : books) {
+                totalVal += b.value;
             }
+        }
+
+        public void sortBooks() {
+            Collections.sort(books);
         }
 
         @Override
@@ -60,6 +82,12 @@ public class ActualOptimiser implements Optimiser {
             l.numDays = inputData.getElementAsInt(i, 1);
             l.numShip = inputData.getElementAsInt(i, 2);
             l.bookIDs = inputData.getRowAsIntList(i + 1);
+            l.books = new HashSet<>();
+
+            for (int id : l.bookIDs) {
+                l.books.add(new Book(id, bookVals.get(id)));
+            }
+
             l.calculateTotalVal();
             libraries.add(l);
         }
@@ -69,6 +97,7 @@ public class ActualOptimiser implements Optimiser {
 
     }
 
+    @SuppressWarnings("unchecked")
     public void algorithm1() {
         Collections.sort(libraries);
         ArrayList<Library> signupOrd = new ArrayList<>();
@@ -84,17 +113,31 @@ public class ActualOptimiser implements Optimiser {
                 break;
             }
         }
-        i = signupOrd.get(0).numDays;
-        int next = 1;
-        ArrayList<Library> signedUp = new ArrayList<>();
-        HashSet<Integer> booksShipped = new HashSet<>();
-        signedUp.add(signupOrd.get(0));
-        for (int d = i; d < maxDays; d++) {
-            if (d >= i + signupOrd.get(next).numDays) {
-                i += signupOrd.get(next).numDays;
-                signedUp.add(signupOrd.get(next++));
+        if (signupOrd.size() > 0) {
+            i = signupOrd.get(0).numDays;
+            int next = 1;
+            ArrayList<Library> signedUp = new ArrayList<>();
+            HashSet<Integer> booksShipped = new HashSet<>();
+            signedUp.add(signupOrd.get(0));
+            for (int d = i; d < maxDays; d++) {
+                if (next < signupOrd.size()) {
+                    if (d >= i + signupOrd.get(next).numDays) {
+                        i += signupOrd.get(next).numDays;
+                        signedUp.add(signupOrd.get(next++));
+                    }
+                }
+                for (Library l : signedUp) {
+                    int ableToShip = l.numShip;
+                    int shipped = 0;
+                    while (shipped < ableToShip && l.bookIDs.size() > 0) {
+                        int id = l.bookIDs.remove(0);
+                        if (!booksShipped.contains(id)) {
+                            booksShipped.add(id);
+                            shipped++;
+                        }
+                    }
+                }
             }
-            for()
         }
         // Step 4: Profit?
         System.out.print(libraries.get(0));
